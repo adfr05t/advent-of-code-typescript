5ce6af7ffac8aabdb135822f974f3d912f332335
Adam Frost <adam.frost1@outlook.com>
2026-07-01 16:36:56 +0100

    feat: solve 2015 day 1 part 2

--- src/2015/day-01/solution.ts
+++ src/2015/day-01/solution.ts
@@ -3,23 +3,38 @@ import {readInput} from "../../utils/readInput.js";
 const filePath = "src/2015/day-01/input.txt";
 const input = readInput(filePath);
 
-console.log(calculateFinalFloor(input));
+const result = calculateFinalFloor(input);
 
-function calculateFinalFloor(input: string): number
+console.log("Part 1:", result.part1);
+console.log("Part 2:", result.part2);
+
+
+function calculateFinalFloor(input: string): {part1: number, part2: number}
 {
     let currentFloor = 0;
+    let basementEntered = false;
+    let charPos = 1110;
 
-    for (const char of input)
+    for (let i = 0; i < input.length; i++)
     {
-        if (char === "(")
+        if (input.charAt(i) === "(")
         {
             currentFloor++;
         }
-        else if (char === ")")
+        else if (input.charAt(i) === ")")
         {
             currentFloor--;
         }
+        
+        if (!basementEntered)
+        {
+            if (currentFloor === -1)
+            {
+                charPos = i + 1;
+                basementEntered = true;
+            }
+        }
     }
 
-    return currentFloor;
+    return {part1: currentFloor, part2: charPos};
 }
