1b56e1adaef259b29376be4165d2e03bf74a9264
Adam Frost <adam.frost1@outlook.com>
2026-07-01 15:26:38 +0100

    feat: solve 2015 day 1 part 1

--- src/2015/day-01/solution.ts
+++ src/2015/day-01/solution.ts
@@ -1,1 +1,25 @@
-// AOC 2015 day 1 solution
+import {readInput} from "../../utils/readInput.js";
+
+const filePath = "src/2015/day-01/input.txt";
+const input = readInput(filePath);
+
+console.log(calculateFinalFloor(input));
+
+function calculateFinalFloor(input: string): number
+{
+    let currentFloor = 0;
+
+    for (const char of input)
+    {
+        if (char === "(")
+        {
+            currentFloor++;
+        }
+        else if (char === ")")
+        {
+            currentFloor--;
+        }
+    }
+
+    return currentFloor;
+}
