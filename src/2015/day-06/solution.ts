69620eff19a5131646291b6a806cbd667232b33f
Adam Frost <adam.frost1@outlook.com>
2026-07-13 18:14:47 +0100

    wip: methods and structure implemented, but not giving correct solution

--- src/2015/day-06/solution.ts
+++ src/2015/day-06/solution.ts
@@ -1,36 +1,170 @@
 import { readInput } from "../../utils/readInput.js";
 
 type Light = {
-    position: {
-        x: number;
-        y: number;
-    },
     isOn: boolean;
 };
 
-// test: create a 10 x 10 grid
-const lightGrid = createGrid(10);
-console.log(lightGrid);
+type GridCoordinates = {
+    from: string;
+    to: string;
+};
 
-function createGrid(size: number): Light[]
+enum Action
 {
-    const lightGrid: Light[] = [];
+    TurnOn = "turn on",
+    TurnOff = "turn off",
+    Toggle = "toggle"
+}
 
-    for (let i = 0; i < size; i++)
+const filePath = "src/2015/day-06/input.txt";
+const input = readInput(filePath);
+
+const lightsOnCount = solvePuzzle(input);
+console.log("Part 1 answer:", lightsOnCount);
+
+function solvePuzzle(input: string): number
+{
+    const lightGrid = createLightGrid(1000);
+    const instructionStrings = input.split(/\r?\n/);
+
+    for (const instructionString of instructionStrings)
     {
-        for (let j = 0; j < size; j++)
-        {
-            const newLight: Light = {
-                position: {
-                    x: i,
-                    y: j
-                },
-                isOn: false
-            }
+        const actionType = getActionType(instructionString);
+        const coordinates = getCoordinates(actionType, instructionString);
 
-            lightGrid.push(newLight);
+        adjustLights(coordinates, actionType, lightGrid);
+    } 
+
+    return lightsTurnedOnCount(lightGrid);
+}
+
+function adjustLights(coordinates: GridCoordinates, actionType: Action, lightGrid: Record<string, Light>)
+{
+    // 1. find span of lights on which to perform action
+    const lightsToChange = affectedLights(coordinates.from, coordinates.to);
+    // 2. apply the action
+    if (actionType === Action.TurnOn)
+    {
+        for (const lightPosition in lightsToChange)
+        {
+            {
+                const light = lightGrid[lightPosition];
+              //  console.log(lightGrid);
+                console.log(lightPosition);
+
+                console.log(light);
+
+               // light.isOn = true;
+            }
+        }
+    }
+    if (actionType === Action.TurnOff)
+    {
+        for (const lightPosition in lightsToChange)
+        {
+            {
+                const light = lightGrid[lightPosition];
+                light.isOn = false;
+            }
+        }
+    }
+    if (actionType === Action.Toggle)
+    {
+        for (const lightPosition in lightsToChange)
+        {
+            {
+                const light = lightGrid[lightPosition];
+                light.isOn = (light.isOn) ? false : true;
+            }
+        }
+    }
+}
+
+function affectedLights(from: string, to: string): string[]
+{
+        const fromCoordinates = from.split(",")
+        const toCoordinates = to.split(",")
+
+        const start = { x: Number(fromCoordinates[0]), y: Number(fromCoordinates[1]) };
+        const end = { x: Number(toCoordinates[0]), y: Number(toCoordinates[1]) };
+
+        let lightsInSelection: string[] = [];
+    
+        for (let y = start.y; y <= end.y; y++) 
+        {
+            const xLimit = (y === end.y) ? end.x : 3; 
+
+            for (let x = start.x; x <= xLimit; x++)
+            {
+                const lightLocationKey = `${x},${y}`;
+                lightsInSelection.push(lightLocationKey);
+            }
+        }
+
+    return lightsInSelection;
+}
+
+function getActionType(instruction: string): Action
+{
+    if (instruction.startsWith(Action.TurnOn))
+    {
+        return Action.TurnOn;
+    }
+    if (instruction.startsWith(Action.TurnOff))
+    {
+        return Action.TurnOff;
+    }
+    
+    return Action.Toggle;
+}
+
+function getCoordinates(actionType: Action, instructionString: string): GridCoordinates
+{
+    const coordinateSection = instructionString.slice(startIndexOfCoordinates(actionType));
+    const coordinates = coordinateSection.split(" through ");
+
+    return { from: coordinates[0], to: coordinates[1] };
+}
+
+function startIndexOfCoordinates(actionType: Action): number
+{
+    if (actionType === Action.TurnOn)
+    {
+        return 8;
+    }
+    if (actionType === Action.TurnOff)
+    {
+        return 9;
+    }
+        return 7;
+}
+
+function createLightGrid(size: number): Record<string, Light>
+{
+    const lightGrid: Record<string, Light> = {};
+
+    for (let x = 0; x < size; x++)
+    {
+        for (let y = 0; y < size; y++)
+        {
+            lightGrid[x + "," + y] = { isOn: false }
         }
     }
 
     return lightGrid;
+}
+
+function lightsTurnedOnCount(lightGrid: Record<string, Light>): number
+{
+    let lightsOnCount = 0;
+
+    for (const light of Object.values(lightGrid))
+    {
+        if (light.isOn)
+        {
+            lightsOnCount++
+        }
+    }
+    
+    return lightsOnCount;
 }
